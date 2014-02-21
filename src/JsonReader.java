e63f2c3b46fb48f517c743f2024f01f5b7132160
Xynnect <dido.jordanov7@gmail.com>
2014-02-21 14:05:30 +0000

    one dimensional is done

--- src/JsonReader.java
+++ src/JsonReader.java
@@ -5,6 +5,7 @@
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Iterator;
 
 import org.json.simple.JSONArray;
@@ -41,10 +42,34 @@ public class JsonReader {
     public static void jsonObjectExtractor(JSONObject objToBeParsedFromFile) {
         i++;
         String questionNumberIndex = "question number ".concat(Integer.toString(i));
-        
+
         JSONObject questionNumber = (JSONObject) objToBeParsedFromFile.get(questionNumberIndex);
         System.out.println(questionNumber);
 
-    }
+        readQuestionType(questionNumber);
+        readQuestionText(questionNumber);
+        readAnswerText(questionNumber);
 
+        System.out.println( readQuestionType(questionNumber));
+        System.out.println(readQuestionText(questionNumber));
+        System.out.println(readAnswerText(questionNumber));
+
+    }
+    public static ArrayList readAnswerText(JSONObject obj) {
+        JSONArray answerTextJsonArray = (JSONArray) obj.get("answerText");
+        ArrayList answerTextArray = new ArrayList();
+        Iterator<String> iterator = answerTextJsonArray.iterator();
+        while (iterator.hasNext()) {
+            answerTextArray.add(iterator.next());
+        }
+        return answerTextArray;
+    }
+    public static String readQuestionText(JSONObject obj) {
+        String questionText = (String) obj.get("questionText");
+        return questionText;
+    }
+    public static String readQuestionType(JSONObject obj) {
+        String questionType = (String) obj.get("questionType");
+        return questionType;
+    }
 }
